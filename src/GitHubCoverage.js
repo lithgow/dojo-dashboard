495c15a59d9875b14cfa4c7791cb45359b529bab
Ben Lithgow Smith <benlithgow@gmail.com>
2020-07-09 15:28:21 +0100

    display instruction coverage

--- src/GitHubCoverage.js
+++ src/GitHubCoverage.js
@@ -14,14 +14,16 @@ class GitHubCoverage extends React.Component {
 
     parseCoverageCounters() {
         const xml = new DOMParser().parseFromString(this.props.xml,"text/xml");
-        const counters = xml.evaluate( '/report/counter', xml, null, XPathResult.ANY_TYPE, null );
+        const counters = xml.evaluate( `/report/counter[@type='INSTRUCTION']`, xml, null, XPathResult.ANY_TYPE, null );
 
         let summary = "";
-        let result = counters.iterateNext();
-        while (result) {
-            const missed = result.getAttribute("missed");
-            summary += missed + " ";
-            result = counters.iterateNext();
+        let instructionCoverage = counters.iterateNext();
+        if (instructionCoverage) {
+            const covered = parseInt(instructionCoverage.getAttribute("covered"));
+            const missed = parseInt(instructionCoverage.getAttribute("missed"));
+            const total = covered + missed;
+            const percentage = Math.floor(covered / total * 100);
+            summary = `${percentage}% (${covered}/${total})`;
         }
         return summary;
     }
