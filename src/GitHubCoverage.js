45b31b3720e34d76a988192a8b357c71e0dc6a58
Ben Lithgow Smith <benlithgow@gmail.com>
2020-07-14 05:47:52 +0100

    coverage tooltip

--- src/GitHubCoverage.js
+++ src/GitHubCoverage.js
@@ -1,38 +1,44 @@
 import React from "react";
 import AssessmentIcon from '@material-ui/icons/Assessment';
+import Tooltip from "@material-ui/core/Tooltip";
 import StyledBadge from "./StyledBadge";
 
 class GitHubCoverage extends React.Component {
 
     render() {
-        let coverage = "-";
-        let coverageIconColor = "disabled";
-        if (this.props.xml) {
-            coverage = this.parseCoverageCounters(this.props.xml);
-            coverageIconColor = "action";
-        }
+        const coverageResults = this.parseCoverageCounters(this.props.xml);
+        const coverage = coverageResults.coverage;
+        const coverageFraction = coverageResults.coverageFraction;
+        const coverageIconColor = coverageResults.coverageIconColor;
         const coverageBadgeColor = coverage > 0 && coverage < 90 ? 'error': 'primary';
         return (
             <StyledBadge badgeContent={`${coverage}%`} color={coverageBadgeColor}>
-                <AssessmentIcon color={coverageIconColor}/>
+                <Tooltip title={coverageFraction}>
+                    <AssessmentIcon color={coverageIconColor}/>
+                </Tooltip>
             </StyledBadge>
         );
     }
 
     parseCoverageCounters() {
-        const xml = new DOMParser().parseFromString(this.props.xml,"text/xml");
-        const counters = xml.evaluate( `/report/counter[@type='INSTRUCTION']`, xml, null, XPathResult.ANY_TYPE, null );
-
         let coverage = "-";
-        let instructionCoverage = counters.iterateNext();
-        if (instructionCoverage) {
-            const covered = parseInt(instructionCoverage.getAttribute("covered"));
-            const missed = parseInt(instructionCoverage.getAttribute("missed"));
-            const total = covered + missed;
-            coverage = Math.floor(covered / total * 100);
-            // coverage = `${percentage}% (${covered}/${total})`;
+        let coverageFraction = "";
+        let coverageIconColor = "disabled";
+        if (this.props.xml) {
+            const xml = new DOMParser().parseFromString(this.props.xml, "text/xml");
+            const counters = xml.evaluate(`/report/counter[@type='INSTRUCTION']`, xml, null, XPathResult.ANY_TYPE, null);
+
+            let instructionCoverage = counters.iterateNext();
+            if (instructionCoverage) {
+                const covered = parseInt(instructionCoverage.getAttribute("covered"));
+                const missed = parseInt(instructionCoverage.getAttribute("missed"));
+                const total = covered + missed;
+                coverage = Math.floor(covered / total * 100);
+                coverageFraction = `${covered} / ${total}`;
+                coverageIconColor = "action";
+            }
         }
-        return coverage;
+        return {coverage, coverageFraction, coverageIconColor};
     }
 }
 
